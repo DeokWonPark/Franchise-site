4a4fc00b45214f9d6a43b4a02dcdf0c49fb4ef51
ejrdnjs <ejrdnjs96@gmail.com>
2021-02-14 21:43:52 +0900

    update image

--- src/components/sidePage/menuBox.jsx
+++ src/components/sidePage/menuBox.jsx
@@ -8,6 +8,10 @@ const MenuBox = ({box}) => {
     const menuBoxRef=useRef(null);
     const menuinfoRef=useRef(null);
     const param=useParams();
+    let path=box.koName;
+    if(param.sort!=="전체 메뉴"){
+        path=param.sort;
+    }
 
     useEffect(()=>{
         if(menuBoxRef.current!==null){
@@ -15,14 +19,16 @@ const MenuBox = ({box}) => {
             if(param.sort!=="전체 메뉴"){
                 menuBoxRef.current.classList.add(styles.into);
                 menuinfoRef.current.style.display="block";
+                path=param.sort;
             }
             else{
                 menuBoxRef.current.classList.remove(styles.into);
                 menuinfoRef.current.style.display="none";
+                path=box.koName;
             }
         }
     },[]);
-    return <Link to={`/MENU/${box.koName}`} className={styles.link}>
+    return <Link to={`/MENU/${path}`} className={styles.link}>
         <div className={styles.menuBox} ref={menuBoxRef}>
             <h2>{box.koName}</h2>
             <h3>{box.enName}</h3>
